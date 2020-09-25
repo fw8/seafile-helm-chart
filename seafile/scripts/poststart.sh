c2cc6fd63ca8b7c2dccc2f4322f30991359c21a4
Florian Wolpert <wolpert@freinet.de>
2020-09-25 11:32:58 +0200

    fix timing issue at initial start and patching url with tls set to false

--- seafile/scripts/poststart.sh
+++ seafile/scripts/poststart.sh
@@ -1,2 +1,3 @@
-sed -i "s+SERVICE_URL = http:.*+SERVICE_URL = https://$SEAFILE_SERVER_HOSTNAME+" /shared/seafile/conf/ccnet.conf
-sed -i "s+FILE_SERVER_ROOT = \"http:.*+FILE_SERVER_ROOT = \"https://$SEAFILE_SERVER_HOSTNAME/seafhttp\"+" /shared/seafile/conf/seahub_settings.py
+while [ ! -f /shared/seafile/conf/ccnet.conf ]; do sleep 1; done
+sed -i "s+SERVICE_URL = http:.*+SERVICE_URL = $SEAFILE_SERVER_SCHEME://$SEAFILE_SERVER_HOSTNAME+" /shared/seafile/conf/ccnet.conf
+sed -i "s+FILE_SERVER_ROOT = \"http:.*+FILE_SERVER_ROOT = \"$SEAFILE_SERVER_SCHEME://$SEAFILE_SERVER_HOSTNAME/seafhttp\"+" /shared/seafile/conf/seahub_settings.py
